b445452a408a83d123e80a3aacfafc09852bd009
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-06-12 21:52:28 +0700

    Search from api

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,10 +21,7 @@ const publicRoutes = [
     component: Upload,
     layout: HeaderOnly,
   },
-  {
-    path: '/profile',
-    component: Profile,
-  },
+  { path: '/@:nickname', component: Profile },
   {
     path: '/search',
     component: Search,
